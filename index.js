0c69be6944f8b1c92ec04c241c13aa5e8c7e2604
Paulo andrade <source.compu@gmail.com>
2018-11-10 10:28:00 -0600

    Se configura puerto y body parser

--- index.js
+++ index.js
@@ -1,1 +1,19 @@
 "use strict"
+
+// Importamos las librerias
+const express = require('express');
+const bodyParser = require('body-parser');
+
+// Creamos una aplicacion de express
+const app = express();
+// Asignamos un puerto
+const port = process.env.port || 3000;
+
+// configuracion de la app
+app.use(bodyParser.urlencoded({extended: false}));
+app.use(bodyParser.json());
+
+// Creamos un servidor y accedemos desde el puerto 3000
+app.listen(port, () => {
+	console.log(`API REST corriendo en el puerto ${port}`);
+});
